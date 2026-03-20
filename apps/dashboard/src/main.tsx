ff17304ef3b2f3c53c41303cf0122b072b75f89d
HuynhThangOfficial <62369063+HuynhThangOfficial@users.noreply.github.com>
2026-03-20 23:25:44 +0700

    4

--- apps/dashboard/src/main.tsx
+++ apps/dashboard/src/main.tsx
@@ -18,8 +18,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     {/* Cập nhật ClerkProvider với domain chính của bạn */}
     <ClerkProvider 
       publishableKey={PUBLISHABLE_KEY}
-      domain="newyas.com"
-      isSatellite={true}
     >
       <ConvexProvider client={convex}>
         <App />
