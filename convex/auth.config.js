5aa42227fdc3a163feb8475ac08023fa73524483
HuynhThangOfficial <62369063+HuynhThangOfficial@users.noreply.github.com>
2026-03-23 14:46:38 +0700

    Update: Change auth domain from falcon to clerk.newyas

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,8 +1,8 @@
 export default {
   providers: [
     {
-      domain: "https://related-falcon-36.clerk.accounts.dev",
-      applicationID: 'convex',
+      domain: "https://clerk.newyas.com", 
+      applicationID: "convex",
     },
   ],
 };
